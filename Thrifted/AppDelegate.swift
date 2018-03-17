5bd0a00f9e36650647bfc57a1f06840158013c2e
Grant Maloney <gpmpn2@mail.missouri.edu>
2018-03-17 15:14:27 -0500

    Facebook SDK Integration, and basic UI Design of the login page. Navigation controller initialized.

--- Thrifted/AppDelegate.swift
+++ Thrifted/AppDelegate.swift
@@ -15,8 +15,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        // Sets shadow (line below the bar) to a blank image
+        UINavigationBar.appearance().shadowImage = UIImage()
+        // Sets the translucent background color
+        UINavigationBar.appearance().backgroundColor = .clear
+        // Set translucent. (Default value is already true, so this can be removed if desired.)
+        UINavigationBar.appearance().isTranslucent = true
         return true
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url as URL!, sourceApplication: sourceApplication, annotation: annotation)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- Thrifted/ViewController.swift
+++ Thrifted/ViewController.swift
@@ -8,11 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
+    let loginButton: FBSDKLoginButton = {
+        let button = FBSDKLoginButton()
+        button.readPermissions = ["email"]
+        return button
+    }()
+    
+    let buttonText = NSAttributedString(string: "Login with Facebook")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "Background")
+        backgroundImage.contentMode =  UIViewContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+        
+        self.navigationItem.title = "Log In"
+        
+        loginButton.setAttributedTitle(buttonText, for: .normal)
+        
+        view.addSubview(loginButton)
+        loginButton.center = view.center
+        loginButton.delegate = self
+        
+        if let token = FBSDKAccessToken.current() {
+            fetchProfile()
+        }
+    }
+    
+    func fetchProfile() {
+        let parameters = ["fields": "email, first_name, last_name, gender, picture.type(large)"]
+        
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start {
+            (connection, result, error) -> Void in
+            
+            guard let result = result as? NSDictionary,
+                let email = result["email"] as? String,
+                let user_name = result["first_name"] as? String,
+                let user_gender = result["gender"] as? String
+            else {
+                return
+            }
+            
+            //Use this when we need to pull the users image data...
+//            if let picture = result["picture"] as? NSDictionary, let data = picture["data"] as? NSDictionary, let url = data["url"] as? String {
+//
+//                if let profileURL = URL(string: url) {
+//
+//                    DispatchQueue.global().async {
+//                        if let data = try? Data(contentsOf: profileURL) {
+//                            DispatchQueue.main.async {
+//                                self.profileImage.image = UIImage(data: data)
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+            
+            print(email)
+        }
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("Completed login")
+        fetchProfile()
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        logoutProfile()
+    }
+    
+    func logoutProfile(){
+        //TODO Logout
+    }
+    
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
     }
 
     override func didReceiveMemoryWarning() {
